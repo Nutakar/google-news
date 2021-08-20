0474eaf80c9a38d8e87f1f658eb07d8869ffd023
Anna Karpunicheva <a.karpunicheva@gmail.com>
2021-08-20 23:16:47 +0300

    Fixed a bug with new_titles list

--- parse.py
+++ parse.py
@@ -7,17 +7,16 @@ query = 'Russia'
 def parse_data(query = query):
     url = 'https://news.google.com/rss/search?q={}+when:1y&hl=en-US&gl=US&ceid=US:en'.format(query)
     news = feedparser.parse(url)
-    news_count = len(news.entries)
+    news_titles = [i.title for i in news.entries]
     separate_words_titles = [str(title).split() for title in news_titles]
     separate_words = []
     for title in separate_words_titles:
         for word in title:
-            separate_words.append(word)    
+            separate_words.append(word)
     return separate_words
 
 def get_top_50(separate_words):
     separate_words = [word for word in separate_words if len(word)>2]
-    print(separate_words)
     dict_of_words = {}
     for word in separate_words:
         if word in dict_of_words:
